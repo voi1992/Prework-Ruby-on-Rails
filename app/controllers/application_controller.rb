019cea6973dfb1a4576f63a9e4cd13ae4fd9fe9f
Tung Nguyen <nguyeentung@gmail.com>
2016-09-13 17:54:27 +0700

    13/9

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller
+
+  protect_from_forgery with: :exception
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:name,:birth])
+  end
+
+  end
+  private
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+end
